d24cf4250fe34ebad919f858a80082ca92cb6c35
DESKTOP-BCGHIPI\홍석기 <nunchaku89@penta.co.kr>
2026-07-14 11:10:52 +0900

    sales_analyze_2

--- projects/sales_analysis/main.py
+++ projects/sales_analysis/main.py
@@ -1,14 +1,26 @@
 from analyze import load_data
+import matplotlib.pyplot as plt
 
 df = load_data("./data/sales.csv")
 
-describe = df.describe()
+head = df.head()
+info = df.info()
+summary = df.describe()
 
 result = f"""
-DESCRIBE : {describe}
-=============================
+==============================
+Sales Dataset Summary
+==============================
+
 행 개수 : {df.shape[0]}
 열 개수 : {df.shape[1]}
+
+기술 통계
+{head}
+==============================
+{info}
+==============================
+{summary}
 """
 
 print(result)
